cfe2a525bf04b196bdf8f44caff74f46394c993b
codersudhir <nandanesudhir1@gmail.com>
2024-01-14 22:17:08 +0530

    hey

--- models/index.js
+++ models/index.js
@@ -4,13 +4,11 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.NODE_ENV) {
+  sequelize = new Sequelize(process.env.NODE_ENV);
 } else {
   sequelize = new Sequelize("mysql://avnadmin:AVNS_LlBufZVMNxo_84Zb_ar@mysql-1148ad12-nandanesudhir1-691a.a.aivencloud.com:14390/defaultdb?ssl-mode=REQUIRED");
 }
